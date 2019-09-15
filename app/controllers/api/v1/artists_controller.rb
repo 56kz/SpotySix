71f577df4dcd9496a911a0ee65c7b73fd7aca73e
khris <khris56@hotmail.com>
2019-09-15 17:07:51 -0500

    first endpoint tested and ordered

--- app/controllers/api/v1/artists_controller.rb
+++ app/controllers/api/v1/artists_controller.rb
@@ -1,8 +1,19 @@
 class Api::V1::ArtistsController < ApplicationController
 
   def index
-    # render json: { data: Artist.all.order(popularity: :desc) }, status: 201
-    @artists = { data: Artist.all.order(popularity: :desc) }
-    json_response(@artists)
+    get_answer
   end
+
+  private
+    def get_answer
+      array = []
+      artist_list = Artist.all.order(popularity: :desc)
+      artist_list.each do |a|
+        new_object = { name: a.name, image: a.image, genres: a.genders, popularity: a.popularity, spotify_url: a.spotify_url, spotify_id: a.spotify_id }
+        array << new_object
+      end
+      answer = { data: array }
+      json_response(answer)
+    end
+
 end
