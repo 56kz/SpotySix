d62d2770d227933eef4e4814ce02de41b343d5f5
khris <khris56@hotmail.com>
2019-09-15 16:38:10 -0500

    first endpoint tested

--- app/controllers/api/v1/artists_controller.rb
+++ app/controllers/api/v1/artists_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::ArtistsController < ApplicationController
 
   def index
-    render json: { data: Artist.all.order(popularity: :desc) }, status: 201
+    # render json: { data: Artist.all.order(popularity: :desc) }, status: 201
+    @artists = Artist.all
+    json_response(@artists)
   end
 end
