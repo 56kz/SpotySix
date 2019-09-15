666434465ba1bc8916ce5ef44c3408e92bb28bbb
khris <khris56@hotmail.com>
2019-09-15 10:52:18 -0500

    random song method improved

--- app/controllers/api/v1/genders_controller.rb
+++ app/controllers/api/v1/genders_controller.rb
@@ -1,7 +1,12 @@
 class Api::V1::GendersController < ApplicationController
   def index
-    gender_id = Gender.find_by(name: params[:genre_name]).id
-    random_song = Song.where(gender_id: gender_id).sample
-    render json: random_song.name
+    gender = Gender.find_by(name: params[:genre_name])
+    if gender == nil || Song.where(gender_id: gender.id).empty?
+      render json: "No hay canciones en este género"
+    else
+      random_song = Song.where(gender_id: gender.id).sample
+      render json: random_song.name
+    end
+
   end
 end
