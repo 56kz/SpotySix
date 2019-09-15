3ba0028bd68220e3b0c30b512eb3527983ec0d25
khris <khris56@hotmail.com>
2019-09-15 16:00:35 -0500

    model spec artists implemented

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,6 @@
 class Artist < ApplicationRecord
-  has_many :relations
-  has_many :genders, through: :relations
+  has_many :albums, dependent: :destroy
+  has_many :relations, dependent: :destroy
+  has_many :genders, through: :relations, dependent: :destroy
+  validates_presence_of :name, :image, :popularity, :spotify_url, :spotify_id
 end
---
+++ spec/models/artist_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+# Test suite for the Todo model
+RSpec.describe Artist, type: :model do
+  # Association test
+  it { should have_many(:albums).dependent(:destroy) }
+  it { should have_many(:genders).dependent(:destroy) }
+  it { should have_many(:relations).dependent(:destroy) }
+  # Validation tests
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:image) }
+  it { should validate_presence_of(:popularity) }
+  it { should validate_presence_of(:spotify_url) }
+  it { should validate_presence_of(:spotify_id) }
+end
